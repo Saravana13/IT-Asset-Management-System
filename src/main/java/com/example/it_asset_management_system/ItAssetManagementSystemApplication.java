f3e7ad2da58013898c9a6b4c59cb5b4716ff0cf8
AK <arunkumar1.ak700@gamil.com>
2025-06-21 13:34:48 +0530

    Base SetUp

---
+++ src/main/java/com/example/it_asset_management_system/ItAssetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.it_asset_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAssetManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItAssetManagementSystemApplication.class, args);
+    }
+
+}
